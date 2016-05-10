6cd7d13d53caf996c58dc12b1b64d31b02101837
Martin <obergma@gmail.com>
2016-05-10 12:25:45 -0500

    feat: learning curve
    Can now make learning curves to assess classifier performance.

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.learning_curve import learning_curve
 from scipy.signal import resample
 from scipy.signal import get_window
 from scipy.signal import spectrogram #, find_peaks_cwt
@@ -339,7 +340,9 @@ def gather_data(data_files):
 
     return X
 
-def split_data(Dat, test_ratio=0.3, X_coi=[], y_coi='', random_state=3):
+def split_data(Dat, actions=[3,4], test_ratio=0.3, X_coi=[], y_coi='', 
+    random_state=3):
+
     if X_coi == [] or y_coi == '':
         print "Specify columns of interest"
         return 0
@@ -353,14 +356,16 @@ def split_data(Dat, test_ratio=0.3, X_coi=[], y_coi='', random_state=3):
     X_test, y_test = pd.DataFrame(), pd.DataFrame([], columns=['act'])
 
     for si in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
-        for ai in [3,4]:
+        for ai in actions:
             d = Dat[(Dat.subj==si) & (Dat.act==ai)]
             n_rows = len(d)
             #print n_rows,
             n_test = int(n_rows*test_ratio)
-            X_train = X_train.append(d[X_coi][:-n_test])
+            n_train = n_rows - n_test
+
+            X_train = X_train.append(d[X_coi][:n_train])
             X_test = X_test.append(d[X_coi][-n_test:])
-            y_train = y_train.append(d[y_coi][:-n_test])
+            y_train = y_train.append(d[y_coi][:n_train])
             y_test = y_test.append(d[y_coi][-n_test:])
             #print len(X_test)
 
@@ -381,11 +386,79 @@ def analysis_first(Dat):
         Dat, X_coi=['pk0', 'pk1', 'pk2'], y_coi=['act'])
     clf = svm.SVC()
     clf.fit(X_train, np.ravel(y_train))
+    print clf
     print clf.score(X_test, y_test)
 
     return clf
 
+def learning_curve(Dat):
+    X, y,_, _= split_data(Dat, test_ratio=0., X_coi=['pk0', 'pk1', 'pk2'], 
+        y_coi=['act'])
+    y = np.ravel(y)
+    plot_learning_curve(svm.SVC(), "SVC Learning Curve", X, y, 
+        actions=[1,2,3,4,5,6,7], train_sizes=np.linspace(.1, 1., 10))
+    plt.show()
 
 def analysis_svm(X_train, y_train, X_test, y_test):
     clf = svm.SVC()
     clf.fit(X_train, y_train)
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
